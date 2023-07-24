b6d75fd5155b8cd94d885b3fb77edb5305e711d0
ollowain86 <83588759+ollowain86@users.noreply.github.com>
2023-07-24 17:12:26 +0200

    finish sfml implementation with test circle

--- nBodySimProject/main.cpp
+++ nBodySimProject/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include "SFML/Graphics.hpp"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Circle");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::White);
+
+        // Create a circle shape
+        sf::CircleShape circle(50.0f);
+        circle.setFillColor(sf::Color::Red);
+        circle.setPosition(375.0f, 275.0f);
+
+        window.draw(circle);
+
+        window.display();
+    }
     return 0;
 }
